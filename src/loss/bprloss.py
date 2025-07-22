b8b4482a3075810471172703f226fee6c0d864ce
aapetukhov <andreybsongs@gmail.com>
2025-07-22 17:24:15 +0300

    upd bpr loss

--- src/loss/bprloss.py
+++ src/loss/bprloss.py
@@ -3,8 +3,12 @@ import torch.functional as F
 
 
 class BPRLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.LogSigmoid()
+
     def forward(self, embedding_user, embedding_item_pos, embedding_item_neg, **batch):
         score_pos = (embedding_user * embedding_item_pos).sum(dim=1)
         score_neg = (embedding_user * embedding_item_neg).sum(dim=1)
-        loss = -F.logsigmoid(score_pos - score_neg).mean()
+        loss = - self.loss(score_pos - score_neg).mean()
         return {"loss": loss}
