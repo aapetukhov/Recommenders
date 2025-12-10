8d08e5ff76d18937fa9646038aed80273a6fcc26
aapetukhov <andreybsongs@gmail.com>
2025-12-10 18:41:43 +0300

    upd rankloss in master

--- src/loss/rankloss.py
+++ src/loss/rankloss.py
@@ -8,8 +8,8 @@ class BPRLoss(nn.Module):
         self.loss = nn.LogSigmoid()
 
     def forward(self, emb_user, emb_item_pos, emb_item_neg, **batch):
-        score_pos = (embedding_user * embedding_item_pos).sum(dim=1)
-        score_new = (embedding_user * embedding_item_neg).sum(dim=1)
+        score_pos = (emb_user * emb_item_pos).sum(dim=1)
+        score_new = (emb_user * emb_item_neg).sum(dim=1)
 
         loss = -self.loss(score_pos - score_new).mean()
         return {"loss": loss}
